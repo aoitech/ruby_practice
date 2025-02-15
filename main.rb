bdc80953f366cb0ec32471f2ef1b3ac01c76f20b
aoitech <aoi19900722@yahoo.co.jp>
2025-02-15 12:08:36 +0900

    privateメソッドの追加

--- main.rb
+++ main.rb
@@ -8,18 +8,21 @@ class Todo
   end
 
   def describe
-    puts "#{title} \n【タスク】\n#{tasks.join("、")}"
+    puts all_task_titles
   end
 
   def add_task(task)
     tasks.push(task)
   end
 
+  private
+  def all_task_titles #クラスのメソッドの中でのみ使用するため、privateメソッド化（インスタンスでは使用しない)
+    "#{title} \n【タスク】\n#{tasks.join("、")}"
+  end
 end
 
 todo = Todo.new("HOME TODO")
 todo.title = "WORK" #def title=の呼び出し。引数"WORK"を渡す
-puts todo.title
 todo.add_task("洗濯")
 todo.add_task("掃除")
 todo.add_task("料理")
